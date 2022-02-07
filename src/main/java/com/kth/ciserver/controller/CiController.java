26be974aadb0400e372fea085bf35878ec8bca9b
Aleks Petkov <aleksp@kth.se>
2022-02-07 20:53:22 +0100

    #5 Executing terminal commands

--- src/main/java/com/kth/ciserver/controller/CiController.java
+++ src/main/java/com/kth/ciserver/controller/CiController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import java.io.*;
 
 @RestController
 public class CiController {
@@ -12,7 +13,6 @@ public class CiController {
 
     @GetMapping()
     public String handleHomepage() {
-
         return "Welcome to our CI server!";
     }
 
@@ -25,6 +25,20 @@ public class CiController {
     }
 
     private void pullAndBuildApplication(GithubWebhookRequest request) {
+        try {
+            String cmd = "git status";
+            Runtime run = Runtime.getRuntime();
+            Process process = run.exec(cmd);
+            process.waitFor();
+            BufferedReader buf = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = buf.readLine()) != null) {
+                System.out.println(line);
+            }
+
+        } catch (IOException | InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
 
     }
 }
