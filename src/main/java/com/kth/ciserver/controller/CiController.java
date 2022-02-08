214f6bd42c3d37215ee643dd3816258a5a70d7f6
Aleks Petkov <aleksp@kth.se>
2022-02-08 13:11:51 +0100

    #5 Building and testing CI-server on POST-requests to server

--- src/main/java/com/kth/ciserver/controller/CiController.java
+++ src/main/java/com/kth/ciserver/controller/CiController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import java.io.*;
+import java.util.ArrayList;
 
 @RestController
 public class CiController {
@@ -25,12 +26,20 @@ public class CiController {
     }
 
     private void pullAndBuildApplication(GithubWebhookRequest request) {
-        executeAndPrintCommand("git branch");
-        executeAndPrintCommand("git checkout main");
-        executeAndPrintCommand("git branch");
+        executeAndPrintCommand("git pull");
+        executeAndPrintCommand(String.format("git checkout %s", request.getHead_commit().getId()));
+        Boolean testsSuccessful = executeAndPrintCommand("./gradlew clean build");
+        System.out.println("Tests successful: " + testsSuccessful); // this should instead be printed to file for consistent storage
     }
 
-    private void executeAndPrintCommand(String cmd) {
+    
+    /** 
+     * @param cmd Command to be executed and printed
+     * @return True if the second word in the second to last line of the standard output 
+     * is "SUCCESSFUL" (indicating a successful test run), else false.
+     */
+    private Boolean executeAndPrintCommand(String cmd) {
+        ArrayList<String> lines = new ArrayList<>();
         try {
             Runtime run = Runtime.getRuntime();
             Process process = run.exec(cmd);
@@ -38,10 +47,13 @@ public class CiController {
             String line;
             while ((line = buf.readLine()) != null) {
                 System.out.println(line);
+                lines.add(line);
             }
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
-
+        if (lines.size() < 2 || lines.get(lines.size()-2).split(" ").length < 2) 
+            return false;
+        return lines.get(lines.size()-2).split(" ")[1].equals("SUCCESSFUL");
     }
 }
